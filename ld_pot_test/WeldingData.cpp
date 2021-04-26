16ed9987dae3c18f5a900cc3db252c60a310fff5
Atte Juvonen <juvonen.atte@gmail.com>
2021-04-26 14:39:42 +0300

    Some old tiny changes, nothing major changed

--- ld_pot_test/WeldingData.cpp
+++ ld_pot_test/WeldingData.cpp
@@ -87,22 +87,22 @@ void WeldingData::fitCurveToData() {
 
 void WeldingData::writeInitialData() {
     double initial_voltages[7] = {  3.4,
-                        4.0,
-                        4.4,
-                        4.8,
-                        5.1,
-                        5.2,
-                        5.3
-                    };
+                                    4.0,
+                                    4.4,
+                                    4.8,
+                                    5.1,
+                                    5.2,
+                                    5.3
+                                };
 
     double initial_feeds[7] = { 6.0,
-                    7.0,
-                    9.0,
-                    12.0,
-                    13.0,
-                    14.0,
-                    15.0
-                };
+                                7.0,
+                                9.0,
+                                12.0,
+                                13.0,
+                                14.0,
+                                15.0
+                            };
 
     Serial.println("writeInitialData");
 
